773e7fa676588061dfd25358ccf7e359fd898c7f
stellamarma <stellamarmat@gmail.com>
2026-05-17 11:44:06 +0300

    Add thetricalshow photos and ts file

---
+++ src/data/theatrico.ts
@@ -0,0 +1,18 @@
+export interface Theatrico {
+  id: number;
+  title: string;
+  category: string;
+  images?: string[]; // Το ερωτηματικό σημαίνει ότι η εικόνα είναι προαιρετική
+  author: string;
+}
+
+export const theatricoData: Theatrico[] = [
+  {
+    id: 1,
+    title: 'Σχετικά με την περίπτωση του αντιδιαβητικού φαρμάκου Actos',
+    category: 'Επιστημονικά Θέματα',
+    images:['/Theatrico/PILATOS_Page_1.jpg','/Theatrico/PILATOS_Page_2.jpg','/Theatrico/PILATOS_Page_3.jpg','/Theatrico/PILATOS_Page_4.jpg','/Theatrico/PILATOS_Page_5.jpg','/Theatrico/PILATOS_Page_6.jpg','/Theatrico/PILATOS_Page_7.jpg','/Theatrico/PILATOS_Page_8.jpg','/Theatrico/PILATOS_Page_9.jpg'],// Διαδρομή εικόνας: public/images/actos.jpg
+    author: 'Αστέρης'
+
+  }
+];
