5c41b63895182a0676c0a73f14ab699e3e6b1c25
stellamarma <stellamarmat@gmail.com>
2026-05-17 12:33:19 +0300

    Change font

--- src/data/theatrico.ts
+++ src/data/theatrico.ts
@@ -9,7 +9,7 @@ export interface Theatrico {
 export const theatricoData: Theatrico[] = [
   {
     id: 1,
-    title: 'Σχετικά με την περίπτωση του αντιδιαβητικού φαρμάκου Actos',
+    title: 'Πιλάτος',
     category: 'Θεατρικο',
     images:['/Theatrico/PILATOS_Page_1.jpg','/Theatrico/PILATOS_Page_2.jpg','/Theatrico/PILATOS_Page_3.jpg','/Theatrico/PILATOS_Page_4.jpg','/Theatrico/PILATOS_Page_5.jpg','/Theatrico/PILATOS_Page_6.jpg','/Theatrico/PILATOS_Page_7.jpg','/Theatrico/PILATOS_Page_8.jpg','/Theatrico/PILATOS_Page_9.jpg'],// Διαδρομή εικόνας: public/images/actos.jpg
     author: 'Αστέρης'
